ef7f184cb5d03bc7ee6f3013f51d86c42bcd78c7
beltram <beltram.maldant@gmail.com>
2025-12-12 12:14:39 +0100

    chore: add comments on StatusListToken claims

--- status-list/src/issuer/cose.rs
+++ status-list/src/issuer/cose.rs
@@ -36,16 +36,28 @@ pub trait StatusListIssuer {
         }
         let unprotected = unprotected_builder.build();
 
-        let mut payload = coset::cwt::ClaimsSetBuilder::new().subject(params.uri.to_string());
+        let mut payload = coset::cwt::ClaimsSetBuilder::new()
+            // Required. As generally defined in [RFC7519].
+            // The sub (subject) claim MUST specify the URI of the Status List Token.
+            // The value MUST be equal to that of the uri claim contained in the status_list claim of the Referenced Token.
+            .subject(params.uri.to_string());
 
         let now = params.artificial_time.unwrap_or_else(elapsed_since_epoch);
         if let Some(expiry) = params.expiry {
-            payload = payload.expiration_time(coset::cwt::Timestamp::WholeSeconds(expiry.to_absolute(now).as_secs() as i64));
+            payload = payload
+                // RECOMMENDED. As generally defined in [RFC7519].
+                // The exp (expiration time) claim, if present, MUST specify the time at which the Status List Token is considered expired by the Status Issuer
+                .expiration_time(coset::cwt::Timestamp::WholeSeconds(expiry.to_absolute(now).as_secs() as i64));
         }
-        payload = payload.issued_at(coset::cwt::Timestamp::WholeSeconds(now.as_secs() as i64));
+        payload = payload
+            // Required. As generally defined in [RFC7519]. The iat (issued at) claim MUST specify the time at which the Status List Token was issued.
+            .issued_at(coset::cwt::Timestamp::WholeSeconds(now.as_secs() as i64));
         if let Some(ttl) = params.ttl {
+            // RECOMMENDED. The ttl (time to live) claim, if present, MUST specify the maximum amount of time, in seconds, that the Status List Token can be cached by a consumer before a fresh copy SHOULD be retrieved.
+            // The value of the claim MUST be a positive number
             payload = payload.claim(CwtClaimName::Ttl, ttl.as_secs().into());
         }
+        // REQUIRED. The status_list (status list) claim MUST specify the Status List conforming to the structure defined in Section 4.2.
         payload = payload.claim(CwtClaimName::StatusList, Value::serialized(&status_list)?);
 
         let payload = payload.build().to_vec()?;
